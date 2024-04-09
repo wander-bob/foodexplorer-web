1cf864d82397539d15c27970474e00851497805f
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 07:18:57 -0300

    feat: Updating main to use routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Routes } from "./routes";
 
 import GlobalStyles from "./styles/global";
-
+import { AuthProvider } from "./hooks/auth";
+import { CartProvider } from "./hooks/cart";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
-    
+    <AuthProvider>
+      <CartProvider>
+        <Routes />
+      </CartProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
