b337466167f1e904e0ccdbfd3133d91a58677846
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 10:08:11 -0300

    fix: Testing case 6 cors origin ommit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-d5co.onrender.com',
+  baseURL: 'https://foodexplorer-api-xqqs.onrender.com',
   withCredentials: true
 });
