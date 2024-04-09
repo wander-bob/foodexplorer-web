be24f51fa1cf29655e15a134491c70c9891a0446
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 08:27:40 -0300

    fix: Updating service api connection to a new domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-3iz9.onrender.com',
+  baseURL: 'https://foodexplorer-api-l9qe.onrender.com',
   withCredentials: true
 });
