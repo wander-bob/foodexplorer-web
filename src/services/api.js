bd1a2c2721ea2ba2b9823a03adb77c86c24fa8ed
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-04-09 07:49:39 -0300

    fix: Updating api service file to public backend domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://foodexplorer-api-3iz9.onrender.com',
   withCredentials: true
 });
